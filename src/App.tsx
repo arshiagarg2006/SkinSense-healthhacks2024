545aaf3dfd4adfc017279a71b30eb3276a6856bc
AadityaMunjal <aadityamunjal06@gmail.com>
2024-10-25 17:13:24 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Find out the price of your life</div>
+    </>
+  );
+}
+
+export default App;
