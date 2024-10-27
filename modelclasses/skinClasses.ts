ccd78516f66b8b1646e391213c10e9a307cc92d4
AadityaMunjal <aadityamunjal06@gmail.com>
2024-10-27 11:49:24 -0400

    add skinClasses

---
+++ modelclasses/skinClasses.ts
@@ -0,0 +1,24 @@
+const SKIN_CLASSES = {
+"0":  "Nail Fungus and other Nail Disease",
+  "1":  "Exanthems and Drug Eruptions",
+  "2":  "Atopic Dermatitis",
+  "3":  "Seborrheic Keratoses and other Benign Tumors",
+  "4":  "Vascular Tumors",
+  "5":  "Melanoma Skin Cancer Nevi and Moles",
+  "6":  "Urticaria Hives",
+  "7":  "Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions",
+  "8":  "Warts Molluscum and other Viral Infections",
+  "9":  "Acne and Rosacea",
+  "10": "Psoriasis pictures Lichen Planus and related diseases",
+  "11":  "Light Diseases and Disorders of Pigmentation",
+  "12":  "Cellulitis Impetigo and other Bacterial Infections",
+  "13":  "Eczema",
+  "14":  "Scabies Lyme Disease and other Infestations and Bites",
+  "15":  "Hair Loss  Alopecia and other Hair Diseases",
+  "16":  "Poison Ivy  and other Contact Dermatitis",
+  "17":  "Vasculitis",
+  "18":  "Lupus and other Connective Tissue diseases",
+  "19":  "Systemic Disease"
+}
+
+export default SKIN_CLASSES;
